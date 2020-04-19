e140c730bf3b2c3fd58ad0e66a0c1923efe0cf5b
ArekWisniewski <59708201+ArekWisniewski@users.noreply.github.com>
2020-04-20 00:12:44 +0200

    Delete cw3.py

--- Cw4/cw3.py
+++
@@ -1,7 +0,0 @@
-with open("DoZad3.txt", "w+") as plik:
-    plik.write(str(print("List")))
-    plik.write(str(print("Listaa")))
-
-with open("DoZad3.txt", "r") as plik:
-    for linia in plik:
-        print(linia, end="")
